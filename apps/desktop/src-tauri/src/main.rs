23c4721f60fbf5899e498fcd5c767116ec1878f6
markbang <i@bangwu.top>
2026-09-18 17:15:09 +0000

    ci: run lint and typecheck before each app build

--- apps/desktop/src-tauri/src/main.rs
+++
@@ -1,5 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    elegant_stack_desktop_lib::run()
-}
--- apps/desktop/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import 'virtual:uno.css'
-
-import { render } from 'solid-js/web'
-import App from './App'
-import './styles.css'
-
-render(() => <App />, document.getElementById('root') as HTMLElement)
